4577fbf29703a91663d1e3e7c6ea9b2ee69668b0
Emmanuelssouza <nao883344@gmail.com>
2025-02-20 17:37:04 -0300

    dsd

--- formularios/cadastro.php
+++ formularios/cadastro.php
@@ -1,12 +1,1 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Aqui você pode adicionar o código para processar os dados do formulário, como salvar no banco de dados
-
-    // Exibir mensagem de sucesso
-    echo "<h1>Dados enviados com sucesso!</h1>";
-} else {
-    // Se o método de requisição não for POST, redirecionar para o formulário
-    header("Location: form001.html");
-    exit();
-}
-?>
+dados emviados com sucesso
